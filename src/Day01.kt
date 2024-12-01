7871fbd4d1a0bc0215a5f050a887a70a277a78d3
Kacper Grabiec <grabiec.kacper01@gmail.com>
2024-12-01 13:26:10 +0100

    Day01 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,54 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left: MutableList<Int> = mutableListOf()
+        val right: MutableList<Int> = mutableListOf()
+
+        input.forEach { pair: String ->
+            val splitted: List<String> = pair.split("   ")
+
+            left.add(splitted[0].toInt())
+            right.add(splitted[1].toInt())
+        }
+
+        var distance = 0
+
+        while (left.isNotEmpty()) {
+            val maxLeft: Int = left.max()
+            left.remove(maxLeft)
+
+            val maxRight: Int = right.max()
+            right.remove(maxRight)
+
+            distance += abs(maxLeft - maxRight)
+        }
+
+        return distance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left: MutableList<Int> = mutableListOf()
+        val right: MutableList<Int> = mutableListOf()
+
+        input.forEach { pair: String ->
+            val splitted: List<String> = pair.split("   ")
+
+            left.add(splitted[0].toInt())
+            right.add(splitted[1].toInt())
+        }
+
+        var score = 0
+        val s: Map<Int, Int> = right.groupingBy { it }.eachCount()
+
+        for (l in left) {
+            score += l * s.getOrDefault(l, 0)
+        }
+
+        return score
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+    println(part2(input))
 }
